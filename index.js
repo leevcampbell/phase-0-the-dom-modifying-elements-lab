5dd26b58e0e17b9934c7fd6a9fb3525e2d36cb89
LeeVincentCampbell <leevcampbell@gmail.com>
2023-02-10 19:09:01 -0500

    added code to index.js to manipulate DOM

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+
+document.getElementById("main").remove();
+
+
+const newHeader = document.createElement("h1");
+const name = prompt("Enter your name: ");
+newHeader.innerHTML = `${name} is the champion`;
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
